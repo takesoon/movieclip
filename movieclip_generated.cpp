239a108f3e93ac750eab1334d3492e685b267fcd
xienannan <xienannan@playcrab.com>
2017-01-10 11:01:04 +0800

    Add        movieclip.fbs Add        movieclip_generated.cpp Add        movieclip_generated.h

---
+++ movieclip_generated.cpp
@@ -0,0 +1,55 @@
+#include "movieclip_generated.h"
+
+#include <vector>
+using namespace std;
+// using namespace MovieClipFlat;
+
+string createFileInfo()
+{
+
+}
+int main()
+{
+  flatbuffers::FlatBufferBuilder builder;
+  /////////// Serialize //////////
+  // Create list 
+  auto filename = builder.CreateString("filename");
+  auto fileUpdateTime = builder.CreateString("fileUpdateTime");
+  auto fileVersion = builder.CreateString("fileVersion");
+
+  auto fileTable = MovieClipFlat::CreateFileHead(builder,1,filename,fileUpdateTime,fileVersion);
+  
+  std::vector<uint64_t> vec;
+
+  std::vector<uint64_t> vec;
+  for(size_t i=0;i<10;i++)
+  {
+    vec.push_back(i);
+  }
+  // Create flat buffer inner type
+  auto id = 123;
+  auto name = builder.CreateString("name");
+  auto list = builder.CreateVector(vec); // vector
+  auto flag = 1;
+  auto kv = KV(1, 1.0); // struct
+  // table
+  auto mloc = CreateTestObj(builder, id, name, flag, list, &kv);
+  builder.Finish(mloc);
+  char* ptr = (char*)builder.GetBufferPointer();
+  uint64_t size = builder.GetSize();
+  
+  ////////// Deserialize //////////
+  auto obj = GetTestObj((uint8_t*)ptr);
+  cout << obj->id() << endl;
+  cout << obj->name()->c_str() << endl;
+  cout << obj->flag() << endl;
+  for(size_t i=0;i<obj->list()->size();i++)
+  {
+    cout << obj->list()->Get(i) << endl;
+  }
+  // can use assign to std::vector for speed up
+  // vec.reserve(obj->list()->size());
+  // vec.assign(obj->list()->begin(), obj->list()->end());
+  cout << obj->kv()->key() << endl;
+  cout << obj->kv()->value() << endl;
+}
